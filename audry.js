a802654b655fc653f6ac74b95f55ae50c00d4322
berkeley platte <berkeleyplatte989@gmail.com>
2019-06-07 15:26:29 -0500

    struggled with a problem which was unknowingly beyond the material taught thus far

---
+++ audry.js
@@ -0,0 +1,18 @@
+function cl(data) {
+  console.log(data);
+}
+
+const container = document.getElementById("container");
+
+// function changeClass(addedeClass) {
+//   container.classList.toggle(addedeClass);
+//   cl("container.classList", container.classList);
+// }
+
+// document.querySelector("body").addEventListener("click", function() {
+//   changeClass("height");
+// });
+
+// document.querySelector("body").addEventListener("scroll", function() {
+//   changeClass("height");
+// });
